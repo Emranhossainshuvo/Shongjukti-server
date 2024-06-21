93c924851d7cddf952b5c108c3f72e0736f476b0
Emranhossainshuvo <emran.shuvo.dev@gmail.com>
2024-06-21 19:47:06 +0600

    added mongodb uri and created a database ans successfully connected with mongodb

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require("express"); 
+const express = require("express");
 const cors = require("cors")
 require("dotenv").config();
 const app = express();
@@ -6,7 +6,29 @@ const port = process.env.PORT || 5000
 
 
 app.use(cors())
-app.use(express.json())
+app.use(express.json());
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.MongoDB_database_userName}:${process.env.MongoDB_database_password}@cluster0.0fn8ke9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+async function run() {
+    try {
+        await client.connect();
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+    }
+}
+run().catch(console.dir);
+
+
 
 app.get("/", (req, res) => {
     res.send("hello from shongjukti");
